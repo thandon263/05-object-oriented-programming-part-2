d7496ea0ecfd092715d934f40ad112e765db4d42
thandon263 <ncubethando20@gmail.com>
2016-06-03 18:10:54 -0400

    The update of receipt adding a fourth instance variable

--- sales_taxe.rb
+++ sales_taxe.rb
@@ -1,66 +1,54 @@
 class Item
 
-    @@counter = 0
+  attr_accessor :name, :price, :sales_tax, :imported
 
-  attr_accessor :name, :price, :imported
-
-  def initialize(name, price, imported)
+  def initialize(name, price, sales_tax, imported)
     @name = name
     @price = price
     @imported = imported
-  end
-
-
-
-      def self.products
-        @@products
-      end
-
-end
-
-class Receipt < Item
-
-
-
-  def initialize(items)
-    @products = items
+    @sales_tax = sales_tax
   end
 
   def add(item)
-    @@counter = @product.length
     @products = []
     @products << item
 
   end
 
+
+end
+
+class Receipt
+
+  def initialize(items)
+    @products = items
+  end
+
   def calculate
     total = 0
     tax = 0
-        @products.each do |product|
-          tax += (product.price * 0.1)
-        end
-        @products.each do |product|
-            total += (product.price + (product.price * 0.1))
-        end
+        @products.each { |product| tax += (product.price * 0.1)}
+        @products.each  { |product| total += (product.price + (product.price * 0.1))}
 
       if @imported == true
         return @products.each { |product| total += (product.price + (product.price * 0.05))}
       end
+
         puts "Taxes: $#{tax}"
+
         "Total: $#{total}"
   end
 
   def output
-    @products.each do |product|
-      puts "#{product.name}: $#{product.price}\n"
-    end
+    @products.each { |product| puts "#{product.name}: $#{product.price}\n"}
   end
+
 end
 
-item1 = Item.new("Bar One chocolate 35g", 15.10, false)
-item2 = Item.new("Bottle of perfume 150g", 102.52, true)
-item3 = Item.new("A packet of Headache pills 10g", 20.42, false)
-item4 = Item.new("A Book of stories", 7.34, true)
+item1 = Item.new("Bar One chocolate 35g", 15.10, false, true)
+item2 = Item.new("Bottle of perfume 150g", 102.52, false, true)
+item3 = Item.new("A packet of Headache pills 10g", 20.42, true, false)
+item4 = Item.new("A Book of stories", 7.34, false, true)
 
 receipt1 = Receipt.new([item1, item2, item3, item4])
 receipt1.output
